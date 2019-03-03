8571e65f55fb834101a05c10181d80581373003a
Brendan Shields <brendan.m.j.shields@gmail.com>
2019-03-03 13:56:31 +1100

    graphQL schema

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const graphqlHttp = require('express-graphql') 
+// destructure to pull certain properties from object
+const { buildSchema } = require('graphql'); 
 // creates express app object;
 const app = express();
 
 // parse JSON information through middleware
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!')
-})
+//graphql config, defining schema, endpoints, resolvers.
+// backticks allow multi-line string
+app.use('/graphql', graphqlHttp({
+    
+    schema: buildSchema(`
+        type RootQuery {
+
+        }
+
+        type RootMutation {
+
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {}
+}));
 //listens on port 3000
 app.listen(3000);
