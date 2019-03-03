bb4cabac659943a3dc5e7ce6a828323f3199409f
Brendan Shields <brendan.m.j.shields@gmail.com>
2019-03-03 19:29:23 +1100

    finished recursive calling

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const graphqlHttp = require('express-graphql')
 // destructure to pull certain properties from object
 const { buildSchema } = require('graphql'); 
 const mongoose = require('mongoose')
-
+const bcrypt = require('bcryptjs')
 const Event = require('./models/event')
+const User = require('./models/user')
 // creates express app object;
 const app = express();
 
@@ -13,6 +14,22 @@ const app = express();
 // parse JSON information through middleware
 app.use(bodyParser.json());
 
+const events = eventIds => {
+    return Event.find({_id: {$in: eventIds}}).then(events => {
+        return events.map(event => {
+            return { ...event._doc, _id: event.id, creator: user.bind(this, event.creator)}
+        })
+    }).catch(error => {
+        throw err;
+    })
+}
+const user = userId => {
+    return User.findById(userId).then(user => {
+        return {...user._doc, _id: user.id, createdEvents: events.bind(this, user._doc.createdEvents) }
+    }).catch(err => {
+        throw err
+    })
+}
 
 //graphql config, defining schema, endpoints, resolvers.
 // backticks allow multi-line string
@@ -27,6 +44,14 @@ app.use('/graphql', graphqlHttp({
             description: String!
             price: Float!
             date: String!
+            creator: User!
+        }
+
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+            createdEvents: [Event!]
         }
 
         input EventInput {
@@ -36,12 +61,18 @@ app.use('/graphql', graphqlHttp({
             date: String!
         }
 
+        input UserInput {
+            email: String!
+            password: String!
+        }
+
         type RootQuery {
-            events: [Event!]!
+            events: [Event!]! 
         }
 
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
 
         schema {
@@ -54,9 +85,10 @@ app.use('/graphql', graphqlHttp({
             return Event.find()
                 .then(events => {
                     return events.map(event => {
-                        return { ...event._doc } //poss convert to string if graphql not reading properly
+                        return { ...event._doc, _id: event.id, creator: user.bind(this, event._doc.creator) } //poss convert to string if graphql not reading properly
                 })
-            }).catch(err => {
+            })
+            .catch(err => {
                 throw err
             })
         },
@@ -65,15 +97,50 @@ app.use('/graphql', graphqlHttp({
             title: args.eventInput.title,
             description: args.eventInput.description,
             price: +args.eventInput.price,
-            date: new Date(args.eventInput.date)
+            date: new Date(args.eventInput.date),
+            creator: "5c7b7025c6f39d7d63229c54" //mongoose auto converts to object id
         })
-        return event.save().then(result => {
-              console.log(result)
-              return {...result._doc}; //spread operator to return only information we want.
-          }).catch(err => {
+        let createdEvent
+        return event
+            .save()
+            .then(result => {
+                createdEvent = {...result._doc, creator: user.bind(this, result._doc.creator)}; //spread operator to return only information we want.
+              return User.findById("5c7b7025c6f39d7d63229c54")
+             
+            })
+            .then(user => {
+                if (!user) {
+                    throw new Error('User does not exist')
+                }
+                user.createdEvents.push(event)
+                return user.save()
+            })
+            .then(result => {
+                return createdEvent
+            })
+            .catch(err => {
               console.log(err)
               throw err;
-          }) //provided by mongoose, connects and saves data to the mongodb
+            }); //provided by mongoose, connects and saves data to the mongodb
+        },
+        createUser: args => {
+        return User.findOne({email: args.userInput.email}).then(user => {
+                if (user) {
+                    throw new Error('User exists already.')
+                }
+                return bcrypt.hash(args.userInput.password, 12)
+            })
+            .then(hashedPassword => {
+                const user = new User({
+                    email: args.userInput.email,
+                    password: hashedPassword
+                });
+                return user.save()             
+            }).then( result => {
+                return {...result._doc, password: null }
+            }).catch(err => {
+                throw err
+            })
         }
      },
      graphiql: true
