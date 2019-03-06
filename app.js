c7c8b9bb57701714ada0ba161cf72a1e180f815a
Brendan Shields <brendan.m.j.shields@gmail.com>
2019-03-06 23:16:50 +1100

    deploy

--- app.js
+++ app.js
@@ -15,6 +15,15 @@ const app = express();
 // parse JSON information through middleware
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200)
+    }
+    next();
+})
 app.use(isAuth)
 
 //graphql config, defining schema, endpoints, resolvers.
